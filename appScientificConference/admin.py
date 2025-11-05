37aaff6afa85986e3ff29081b8e87418997368ea
leirelarrauri <larrauri.leire@opendeusto.es>
2025-11-05 19:51:26 +0100

    Change models.py

--- appScientificConference/admin.py
+++ appScientificConference/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Departamento, Habilidad, Empleado
-admin.site.register(Departamento)
-admin.site.register(Habilidad)
-admin.site.register(Empleado)
+from .models import Articulo, Autor, Track
+admin.site.register(Articulo)
+admin.site.register(Autor)
+admin.site.register(Track)
--- appScientificConference/models.py
+++ appScientificConference/models.py
@@ -1,21 +1,15 @@
 from django.db import models
- 
-class Departamento(models.Model):
-    # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
-    nombre = models.CharField(max_length=50)
-    telefono = models.IntegerField()
+# Ejemplo Django
+class Track(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
 
-class Habilidad(models.Model):
-    # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
-    nombre = models.CharField(max_length=50)
- 
-class Empleado(models.Model):
-    # Campo para la relación one-to-many (un empleado pertenece a un departamento)
-    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-    # Campo para la relación many-to-many (un empleado tiene varias habilidades)
-    habilidades = models.ManyToManyField(Habilidad)
-    nombre = models.CharField(max_length=40)
-    fecha_nacimiento = models.DateField()
-    # Es posible indicar un valor por defecto mediante 'default'
-    antiguedad = models.IntegerField(default=0)
-    # Para permitir propiedades con valor null, añadiremos las opciones null=True, blank=True.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    afiliacion = models.CharField(max_length=200, blank=True)
+
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=200)
+    abstract = models.TextField()
+    track = models.ForeignKey(Track, on_delete=models.CASCADE, related_name="articulos")
+    autores = models.ManyToManyField(Autor, related_name="articulos")
