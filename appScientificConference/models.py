544b4d8342f1feaabaf65a93a332523de651e894
leirelarrauri <larrauri.leire@opendeusto.es>
2025-11-06 17:49:11 +0100

    Crear plantillas basicas

--- appScientificConference/models.py
+++ appScientificConference/models.py
@@ -1,15 +1,26 @@
 from django.db import models
-# Ejemplo Django
+
 class Track(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
 
+    def __str__(self):
+        return self.nombre
+
+
 class Autor(models.Model):
     nombre = models.CharField(max_length=100)
     afiliacion = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
 
 class Articulo(models.Model):
     titulo = models.CharField(max_length=200)
     abstract = models.TextField()
     track = models.ForeignKey(Track, on_delete=models.CASCADE, related_name="articulos")
     autores = models.ManyToManyField(Autor, related_name="articulos")
+
+    def __str__(self):
+        return self.titulo
--- appScientificConference/views.py
+++ appScientificConference/views.py
@@ -1,35 +1,43 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import render
 from .models import Articulo, Autor, Track
 
-# Devuelve el listado de tracks (CORRECTO)
+# Devuelve el listado de tracks 
 def index_tracks(request):
     tracks = get_list_or_404(Track.objects.order_by('nombre'))
-    output = ', '.join([t.nombre for t in tracks])
-    return HttpResponse(output)
+    context = {'lista_tracks': tracks }
+    return render(request, 'index.html', context)
 
-# Devuelve los datos de un track (CORRECTO)
+
+
+# Devuelve los datos de un track 
 def show_track(request, track_id):
     track = get_object_or_404(Track, pk=track_id)
     output = f'Detalles del track: {track.id}, {track.nombre}, {track.descripcion}'
     return HttpResponse(output)
 
-# Devuelve los autores de un artículo (CORREGIDO)
+def show_track(request, track_id):
+    track = get_object_or_404(Track, id=track_id)
+    articulos = track.articulos.all().order_by('titulo')
+    return render(request, 'track.html', {
+        'track': track,
+        'articulos': articulos
+    })
+# Devuelve los autores de un artículo
 def index_autores_articulo(request, articulo_id):
     articulo = get_object_or_404(Articulo, pk=articulo_id)
-    # Usar 'autores' en lugar de 'autor_set' porque la relación se llama 'autores'
     output = ', '.join([a.nombre for a in articulo.autores.all()])
     return HttpResponse(output)
 
-# Devuelve los detalles de un autor (CORREGIDO)
+# Devuelve los detalles de un autor 
 def show_autor(request, autor_id):
     autor = get_object_or_404(Autor, pk=autor_id)
     output = f'Detalles del autor: {autor.id}, {autor.nombre}, {autor.afiliacion}'
     return HttpResponse(output)
 
-# Devuelve los detalles de un artículo (CORREGIDO)
+# Devuelve los detalles de un artículo 
 def show_articulo(request, articulo_id):
     articulo = get_object_or_404(Articulo, pk=articulo_id)
-    # Corregir el nombre del campo y usar 'autores' en lugar de 'autor_set'
     output = f'Detalles del artículo: {articulo.id}, {articulo.titulo}. Autores: {[a.nombre for a in articulo.autores.all()]}'
     return HttpResponse(output)
