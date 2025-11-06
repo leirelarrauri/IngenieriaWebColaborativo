84fc71310775435c7b7fe315650233025cb317b4
leirelarrauri <larrauri.leire@opendeusto.es>
2025-11-06 22:33:48 +0100

    Modify views

--- appScientificConference/views.py
+++ appScientificConference/views.py
@@ -3,41 +3,74 @@ from django.shortcuts import get_object_or_404, get_list_or_404
 from django.shortcuts import render
 from .models import Articulo, Autor, Track
 
-# Devuelve el listado de tracks 
-def index_tracks(request):
-    tracks = get_list_or_404(Track.objects.order_by('nombre'))
-    context = {'lista_tracks': tracks }
+
+#Devuelve la pagina principal
+def index(request):
+    # Obtener todos los tracks ordenados
+    tracks = Track.objects.all().order_by('nombre')
+    
+    # Crear una lista de tuplas (track, articulo_destacado)
+    tracks_con_articulo = []
+    
+    for track in tracks:
+        # Eliges el primer articulo ordenado por titulo
+        articulo_destacado = track.articulos.order_by('titulo').first()
+        
+        tracks_con_articulo.append({
+            'track': track,
+            'articulo_destacado': articulo_destacado
+        })
+    
+    context = {
+        'tracks_con_articulo': tracks_con_articulo
+    }
     return render(request, 'index.html', context)
 
+# Devuelve el listado de tracks 
+def lista_tracks(request):
+    tracks = get_list_or_404(Track.objects.order_by('nombre'))
+    return render(request, 'tracks.html', {
+        'lista_tracks': tracks
+    })
 
+# Devuelve el listado de autores 
+def lista_autores(request):
+    autores = get_list_or_404(Autor.objects.order_by('nombre'))
+    return render(request, 'autores.html', {
+        'lista_autores': autores
+    })
 
-# Devuelve los datos de un track 
-def show_track(request, track_id):
-    track = get_object_or_404(Track, pk=track_id)
-    output = f'Detalles del track: {track.id}, {track.nombre}, {track.descripcion}'
-    return HttpResponse(output)
+# Devuelve el listado de articulos 
+def lista_articulos(request):
+    articulos = get_list_or_404(Articulo.objects.order_by('nombre'))
+    return render(request, 'articulos.html', {
+        'lista_articulos': articulos
+    })
 
+# Devuelve los detalles de un track 
 def show_track(request, track_id):
     track = get_object_or_404(Track, id=track_id)
-    articulos = track.articulos.all().order_by('titulo')
+    articulos = get_list_or_404(track.articulos.all().order_by('titulo'))
     return render(request, 'track.html', {
         'track': track,
         'articulos': articulos
     })
-# Devuelve los autores de un artículo
-def index_autores_articulo(request, articulo_id):
-    articulo = get_object_or_404(Articulo, pk=articulo_id)
-    output = ', '.join([a.nombre for a in articulo.autores.all()])
-    return HttpResponse(output)
-
 # Devuelve los detalles de un autor 
 def show_autor(request, autor_id):
     autor = get_object_or_404(Autor, pk=autor_id)
-    output = f'Detalles del autor: {autor.id}, {autor.nombre}, {autor.afiliacion}'
-    return HttpResponse(output)
+    articulos = autor.articulos.all().order_by('titulo')
+    return render(request, 'autor.html', {
+        'autor': autor,
+        'articulos': articulos
+    })
 
 # Devuelve los detalles de un artículo 
 def show_articulo(request, articulo_id):
     articulo = get_object_or_404(Articulo, pk=articulo_id)
-    output = f'Detalles del artículo: {articulo.id}, {articulo.titulo}. Autores: {[a.nombre for a in articulo.autores.all()]}'
-    return HttpResponse(output)
+    track = get_object_or_404(Track, pk=articulo.track.id)
+    autores = get_list_or_404(articulo.autores.all().order_by('titulo'))    
+    return render(request, 'articulo.html', {
+        'articulo': articulo,
+        'track': track,
+        'autores': autores
+    })
