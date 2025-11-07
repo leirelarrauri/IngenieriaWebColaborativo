274193e0b884e6b3cfa681649bd4a59a3609ac67
leirelarrauri <larrauri.leire@opendeusto.es>
2025-11-07 10:48:18 +0100

    Update plantillas

--- appScientificConference/urls.py
+++ appScientificConference/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tracks/', views.index_tracks, name='index_tracks'),
+    path('index/', views.index, name='index'),
+    path('tracks/', views.lista_tracks, name='lista_tracks'),
     path('tracks/<int:track_id>/', views.show_track, name='show_track'),
-    path('articulos/<int:articulo_id>/autores/', views.index_autores_articulo, name='index_autores_articulo'),
+    path('autores/', views.lista_autores, name='lista_autores'),
     path('autores/<int:autor_id>/', views.show_autor, name='show_autor'),
+    path('articulos/', views.lista_articulos, name='lista_articulos'),
     path('articulos/<int:articulo_id>/', views.show_articulo, name='show_articulo'),
 ]
